6913fff6ef6c51879b3d42ad6646b3d774c38712
sunhesh12 <hsdbandaranayake@gmail.com>
2024-06-15 21:43:31 +0530

    First Commit

---
+++ src/DbConnection/DBConnection_1.java
@@ -0,0 +1,35 @@
+package DbConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection_1 {
+    private static DBConnection_1 dbConnection;
+
+    private final Connection connection;
+
+    private DBConnection_1() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/StudentApplication","root","wakudabarnp123");
+    
+    }
+
+    /**
+     *
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public static DBConnection_1 getInstance() throws SQLException, ClassNotFoundException {
+        /*if(dbConnection==null){
+            dbConnection=new DBConnection();
+        }
+        return dbConnection;*/
+        return  (dbConnection==null)?dbConnection=new DBConnection_1() :dbConnection;
+    }
+
+    public Connection getConnection(){
+        return  connection;
+    }
+}
